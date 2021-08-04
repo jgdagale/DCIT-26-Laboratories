0ed9bea42c93ea5051eedbd14551a8ce579c1466
FirstLaboratory <jeremiasdagale08@gmail.com>
2021-08-04 17:09:20 +0800

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">{{ config('app.name', 'Laravel') }}</a>
+        </div>
+    
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="nav navbar-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/createpost">Add Post</a></li>
+                <li><a href="/viewpost">View Post</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Create Post</h1>
+    {!! Form::open(['action' => 'App\Http\Controllers\PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    <div class="form-group">
+        {{Form::label('title', 'Title')}}
+        {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+    </div>
+    <br>
+    <div class="form-group">
+        {{Form::label('body', 'Body')}}
+        {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder' => 'Body Text'])}}
+    </div>
+    <br>
+    {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+
+{!! Form::close() !!}
+@endsection
